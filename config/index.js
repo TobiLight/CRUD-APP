7d00f76e1ea00bb835ddb64b995623cb461aaeac
Xemicolon <1vibenation@gmail.com>
2021-05-08 18:02:28 -0500

    Updated readme

--- config/index.js
+++ config/index.js
@@ -1,15 +1,3 @@
-const dotenv = require("dotenv").config();
-
-// process.env.NODE_ENV = process.env.NODE_ENV || "development";
-
-const envFound = dotenv;
-if (envFound.error) {
-  // This error should crash whole process
-  console.log(envFound.error);
-  // throw new Error("⚠️  Couldn't find .env file  ⚠️");
-  throw new Error(envFound.error);
-}
-
 const config = {
   // PORT GOES HERE
   port: parseInt(process.env.PORT, 10),
