430e84c5024339d8d4eb52d745857b5ca63a3db9
Xemicolon <1vibenation@gmail.com>
2021-05-08 18:03:21 -0500

    chore: code refactor

--- config/index.js
+++ config/index.js
@@ -1,12 +1,11 @@
 const config = {
-  // PORT GOES HERE
+  // PORT
   port: parseInt(process.env.PORT, 10),
   // DATABASE URL
   databaseURL:
     process.env.NODE_ENV === "production"
       ? process.env.MONGO_URL
       : process.env.MONGO_URI,
-  //   JWT SECRET
 };
 
 module.exports = config;
