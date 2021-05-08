72556e510f742f4c34c9f3da21f4013d38514359
Xemicolon <1vibenation@gmail.com>
2021-05-08 17:39:20 -0500

    Updated readme

--- config/index.js
+++ config/index.js
@@ -2,9 +2,12 @@ const dotenv = require("dotenv");
 
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
+console.log(process.env.NODE_ENV);
+
 const envFound = dotenv.config();
 if (envFound.error) {
   // This error should crash whole process
+  console.log(envFound.error);
 
   throw new Error("⚠️  Couldn't find .env file  ⚠️");
 }
